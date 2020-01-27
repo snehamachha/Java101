0425424f33864230403581965fd08f3a72cda9b7
snehamachha <60336189+snehamachha@users.noreply.github.com>
2020-01-27 00:56:44 -0500

    second commit

--- src/SampleProgram.java
+++ src/SampleProgram.java
@@ -5,9 +5,9 @@ public class SampleProgram {
 		for (int j = 1; j <= 4; j++) {
 			for (int i = 1; i <= 4; i++) {
 				if (i == 1||i==4||j==1||j==4)
-					System.out.print("$");
+					System.out.print("$ ");
 				else
-					System.out.print(" ");
+					System.out.print("  ");
 			}
 			System.out.println();
 		}
